140dd52d337af4bbafe5cf3cfa687d36ced36789
Lane Bretschneider <lanebret85@gmail.com>
2023-06-27 14:23:29 -0600

    add setup comments including overall goal and pseudocode

---
+++ challenge1.rb
@@ -0,0 +1,9 @@
+# Given an array of strings, print only the strings that have exactly 4 characters.
+
+# In my words:
+# Write a program that takes an array of elements of type string and print those elements that have exactly 4 characters, no more, no less.
+
+# Pseudocode:
+# 1. Create an array that holds some number of elements of type string
+# 2. Call the .each method on the array that uses a conditional to print each string element only if that element has exactly 4 characters
+

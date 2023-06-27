81108231c0aa4149bb4998e728c4ab262694b34d
Lane Bretschneider <lanebret85@gmail.com>
2023-06-27 15:00:46 -0600

    add pseudocode to challenge2.rb

--- challenge2.rb
+++ challenge2.rb
@@ -4,4 +4,8 @@
 # Write a program that takes an array that contains string elements, where each word has some combination of uppercase and lowercase letters. Use the program to print out each word in all lowercase letters. 
 
 # Pseudocode:
-# 
+# 1. Create an array containing some number of elements of type string that are a mix of uppercase and lowercase characters
+# 2. Call the .each method on the array to iterate through each element of it
+# 3. Within that .each method, call the .downcase method on each element and print the return value using a puts statement
+
+# Code:

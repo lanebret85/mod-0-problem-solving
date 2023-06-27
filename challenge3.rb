b0421363a8306bfbe73239a9bcf918a8605c873a
Lane Bretschneider <lanebret85@gmail.com>
2023-06-27 15:40:39 -0600

    add setup comments

---
+++ challenge3.rb
@@ -0,0 +1,11 @@
+# Write a method or function that removes all instances of the letter <strong>s</strong> in a string. The method or function should accept a string as an argument and return the same string with every instance of the letter <strong>s</strong> removed.
+
+# In my words:
+# Take a string and write a method that will accept the string as an input and return that same string without any of the "s" characters in it
+
+# Pseudocode:
+# 1. Create a variable and assign a string value to it.
+# 2. Create a custom method with one parameter that will take a string argument.
+# 3. Inside the method, call the .delete method on the string argument from the custom method. The .delete method takes one parameter, and I will enter the argument "s".
+# 4. Call the custom method on the variable that contains the string value.
+
